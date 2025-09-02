597192b9025ada6e17490f75849476defd6632eb
Mehdi <mehdicontacto@gmail.com>
2025-09-02 10:13:30 +0300

    Install React and started server

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+const express = require("express")
+
+
+const app = express()
+const port = process.env.PORT
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>This is the Social Networking APP</div>
+  )
+}
+
+export default App
