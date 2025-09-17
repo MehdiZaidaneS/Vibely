15a632297eefd1479e36136d135f86e50b25f383
Osama-Aamer <osamaaamer2@gmail.com>
2025-09-17 23:44:46 +0300

    update App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import EventPage from "./pages/EventPage"; // Assuming EventPage is in pages/ folder
+import EventPage from "./pages/EventPage"; 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* Add EventPage below or as a separate section; no routing needed if not navigating */}
       <EventPage />
     </div>
   );
