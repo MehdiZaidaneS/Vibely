286783112839cecbcc8e032b1c3dc9acab1812c9
Mehdi <mehdicontacto@gmail.com>
2025-09-25 14:02:59 +0300

    Commenting couple of lines to prevent conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./components/auth/authPage";
 import WelcomePage from "./components/auth/welcomePage";
 import EventPage from "./pages/EventPage";
-import CreateEvent from "./pages/CreateEvent";
+// import CreateEvent from "./pages/CreateEvent";
 import PublicChat from "./components/chat/publicChat";
 import "./App.css";
 
@@ -14,7 +14,7 @@ function App() {
         <Route path="auth" element={<Auth />} />
         <Route path="welcome" element={<WelcomePage />} />
         <Route path="events" element={<EventPage />} />
-        <Route path="create-event" element={<CreateEvent />} />
+        {/* <Route path="create-event" element={<CreateEvent />} /> */}
         <Route path="/chat" element={<PublicChat />} />
       </Routes>
     </BrowserRouter>
