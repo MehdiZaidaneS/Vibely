19b4faebfada066f95379c9d8b1e13620efd5ca1
Jesperho <jesperho@metropolia.fi>
2025-09-28 12:51:16 +0300

    Implemented private chat + small changes to public chat. Added navigation within chats

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Auth from "./components/auth/authPage";
 import WelcomePage from "./components/auth/welcomePage";
 import EventPage from "./pages/EventPage";
 import CreateEvent from "./pages/CreateEvent";
 import PublicChat from "./components/chat/publicChat";
+import PrivateChat from "./components/chat/privateChat";
 import "./App.css";
 
 function App() {
@@ -13,9 +14,10 @@ function App() {
       <Routes>
         <Route path="auth" element={<Auth />} />
         <Route path="welcome" element={<WelcomePage />} />
-        <Route path="events" element={<EventPage />} />
+        <Route path="/" element={<EventPage />} />
         <Route path="create-event" element={<CreateEvent />} />
-        <Route path="/chat" element={<PublicChat />} />
+        <Route path="/public-chat" element={<PublicChat />} />
+        <Route path="/private-chat" element={<PrivateChat />} />
       </Routes>
     </BrowserRouter>
   );
