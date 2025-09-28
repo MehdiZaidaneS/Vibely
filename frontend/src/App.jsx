5c4981a38a1bdb5ecaf7cd4dd3aab2341c1107e9
Jesperho <jesperho@metropolia.fi>
2025-09-28 13:10:35 +0300

    Added private chat route ton app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,30 @@ import WelcomePage from "./components/auth/welcomePage";
 import EventPage from "./pages/EventPage";
 // import CreateEvent from "./pages/CreateEvent";
 import PublicChat from "./components/chat/publicChat";
+import PrivateChat from "./components/chat/privateChat";
 import "./App.css";
-import React, { useState } from "react"; 
-
+import React, { useState } from "react";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(
     localStorage.getItem("user") || false
   );
   return (
-
     <BrowserRouter>
       <Routes>
         <Route
-              path="/"
-              element={isAuthenticated ? <EventPage /> : <Navigate to="/auth" />}
-            />
+          path="/"
+          element={isAuthenticated ? <EventPage /> : <Navigate to="/auth" />}
+        />
         <Route path="/auth" element={<Auth />} />
         <Route path="/welcome" element={<WelcomePage />} />
         <Route path="/events" element={<EventPage />} />
 
         {/* <Route path="create-event" element={<CreateEvent />} /> */}
-        <Route path="/chat" element={<PublicChat />} />
+        <Route path="/public-chat" element={<PublicChat />} />
+        <Route path="/private-chat" element={<PrivateChat />} />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
