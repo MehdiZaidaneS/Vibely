f68604d61872967608275185019ef485926b8e89
Jesperho <jesperho@metropolia.fi>
2025-09-28 13:07:18 +0300

    Fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./components/auth/authPage";
 import WelcomePage from "./components/auth/welcomePage";
 import EventPage from "./pages/EventPage";
 import CreateEvent from "./pages/CreateEvent";
 import PublicChat from "./components/chat/publicChat";
-import PrivateChat from "./components/chat/privateChat";
 import "./App.css";
 
 function App() {
@@ -14,10 +13,9 @@ function App() {
       <Routes>
         <Route path="auth" element={<Auth />} />
         <Route path="welcome" element={<WelcomePage />} />
-        <Route path="/" element={<EventPage />} />
+        <Route path="events" element={<EventPage />} />
         <Route path="create-event" element={<CreateEvent />} />
-        <Route path="/public-chat" element={<PublicChat />} />
-        <Route path="/private-chat" element={<PrivateChat />} />
+        <Route path="/chat" element={<PublicChat />} />
       </Routes>
     </BrowserRouter>
   );
